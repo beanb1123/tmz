61f04fe73f1c4d9bef9d5fdef0cf15ab787f3e44
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2024-10-06 21:15:35 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-curl -L https://raw.githubusercontent.com/beanb1123/tmz/refs/heads/main/tm.sh -o tm.sh && chmod +x tm.sh && bash tm.sh &
+sudo apt update
+sudo apt install docker.io -y
+docker pull traffmonetizer/cli_v2:latest
+docker run -i --name tm traffmonetizer/cli_v2 start accept --token iprphzS8Lr+JnTZrepEy1cIV3Z4vVJOxHHTgGph10mI=
