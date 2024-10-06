7ea7b0ef061061cfa729613067e7673d3eefa401
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2024-10-06 21:01:45 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,1 @@
-curl -L https://raw.githubusercontent.com/beanb1123/tmz/refs/heads/main/tm.sh -o tm.sh
-chmod +x tm.sh
-nohup bash tm.sh &
-tail -f nohup.out &
+curl -L https://raw.githubusercontent.com/beanb1123/tmz/refs/heads/main/tm.sh -o tm.sh && chmod +x tm.sh && bash tm.sh &
